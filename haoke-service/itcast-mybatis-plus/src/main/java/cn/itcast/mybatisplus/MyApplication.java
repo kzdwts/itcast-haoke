167e7c0e4cbb6c71268de6fac4cce7d41bbe96b8
kangyong <kangyong_wts@sina.com>
2022-03-03 23:23:35 +0800

    初始化MybatisPlus测试工程

---
+++ haoke-service/itcast-mybatis-plus/src/main/java/cn/itcast/mybatisplus/MyApplication.java
@@ -0,0 +1,22 @@
+package cn.itcast.mybatisplus;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author Kang Yong
+ * @date 2022/3/3
+ * @since 1.0.0
+ */
+@MapperScan(basePackages = {"cn.itcast.mybatisplus.mapper"})
+@SpringBootApplication
+public class MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+}
---
+++ haoke-service/itcast-mybatis-plus/src/main/java/cn/itcast/mybatisplus/pojo/User.java
@@ -0,0 +1,16 @@
+package cn.itcast.mybatisplus.pojo;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * 用户
+ *
+ * @author Kang Yong
+ * @date 2022/3/3
+ * @since 1.0.0
+ */
+@Data
+@ToString
+public class User {
+}
