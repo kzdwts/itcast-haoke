eed42d3c41a906ddc1709bee3aeb16694f58b3a0
kangyong <kangyong_wts@sina.com>
2022-03-05 23:22:35 +0800

    feat: 更新房源查询相关文件

---
+++ haoke-service/itcast-haoke-manage/itcast-haoke-manage-dubbo-server/itcast-haoke-manage-dubbo-server-house-resources/itcast-haoke-manage-dubbo-server-house-resources-dubbo-service/src/main/java/cn/itcast/haoke/dubbo/server/api/ApiHouseResourcesServiceImpl.java
@@ -0,0 +1,33 @@
+package cn.itcast.haoke.dubbo.server.api;
+
+import cn.itcast.haoke.dubbo.server.pojo.HouseResources;
+import cn.itcast.haoke.dubbo.server.service.HouseResourcesService;
+import com.alibaba.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * 房源管理
+ *
+ * @author Kang Yong
+ * @date 2022/3/5
+ * @since 1.0.0
+ */
+@Service(version = "1.0.0") // 这是dubbo服务，对外进行暴露
+public class ApiHouseResourcesServiceImpl implements ApiHouseResourcesService {
+
+    @Autowired
+    private HouseResourcesService houseResourcesService;
+
+    /**
+     * 保存房源信息
+     *
+     * @param houseResources {@link HouseResources}
+     * @return {@link int} -1:输入的参数不符合要求，0：数据插入数据库失败，1：成功
+     * @author Kang Yong
+     * @date 2022/3/5
+     */
+    @Override
+    public int saveHouseResources(HouseResources houseResources) {
+        return this.houseResourcesService.saveHouseResources(houseResources);
+    }
+}
