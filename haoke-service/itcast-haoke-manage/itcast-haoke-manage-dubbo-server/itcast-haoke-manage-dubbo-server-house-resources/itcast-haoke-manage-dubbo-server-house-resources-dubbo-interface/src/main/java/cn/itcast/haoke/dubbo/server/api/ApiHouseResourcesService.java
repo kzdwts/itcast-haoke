1b8bc56fc52d07dabeba06db28b30593d83c2495
kangyong <kangyong_wts@sina.com>
2022-03-06 16:39:58 +0800

    feat: 新增房源表实体类

--- haoke-service/itcast-haoke-manage/itcast-haoke-manage-dubbo-server/itcast-haoke-manage-dubbo-server-house-resources/itcast-haoke-manage-dubbo-server-house-resources-dubbo-interface/src/main/java/cn/itcast/haoke/dubbo/server/api/ApiHouseResourcesService.java
+++ haoke-service/itcast-haoke-manage/itcast-haoke-manage-dubbo-server/itcast-haoke-manage-dubbo-server-house-resources/itcast-haoke-manage-dubbo-server-house-resources-dubbo-interface/src/main/java/cn/itcast/haoke/dubbo/server/api/ApiHouseResourcesService.java
@@ -1,11 +1,23 @@
 package cn.itcast.haoke.dubbo.server.api;
 
+import cn.itcast.haoke.dubbo.server.pojo.HouseResources;
+
 /**
- * TODO desc
+ * 房源信息 业务接口层 gegnxin
  *
  * @author Kang Yong
- * @date 2022/3/6
+ * @date 2022/3/5
  * @since 1.0.0
  */
-public class ApiHouseResourcesService {
+public interface ApiHouseResourcesService {
+
+    /**
+     * 保存房源信息
+     *
+     * @param houseResources {@link HouseResources}
+     * @return {@link int} -1:输入的参数不符合要求，0：数据插入数据库失败，1：成功
+     * @author Kang Yong
+     * @date 2022/3/5
+     */
+    int saveHouseResources(HouseResources houseResources);
 }
