3fb7271458fe9427fdc77b1e121677566624aee2
kangyong <kangyong_wts@sina.com>
2022-03-06 16:28:40 +0800

    feat: 初始化项目

---
+++ haoke-service/itcast-haoke-manage/itcast-haoke-manage-dubbo-server/itcast-haoke-manage-dubbo-server-house-resources/itcast-haoke-manage-dubbo-server-house-resources-dubbo-interface/src/main/java/cn/itcast/haoke/dubbo/server/api/ApiHouseResourcesService.java
@@ -0,0 +1,11 @@
+package cn.itcast.haoke.dubbo.server.api;
+
+/**
+ * TODO desc
+ *
+ * @author Kang Yong
+ * @date 2022/3/6
+ * @since 1.0.0
+ */
+public class ApiHouseResourcesService {
+}
---
+++ haoke-service/itcast-haoke-manage/itcast-haoke-manage-dubbo-server/itcast-haoke-manage-dubbo-server-house-resources/itcast-haoke-manage-dubbo-server-house-resources-dubbo-interface/src/main/java/cn/itcast/haoke/dubbo/server/api/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * TODO desc
+ *
+ * @author Kang Yong
+ * @date 2022/3/6
+ * @since 1.0.0
+ */
+package cn.itcast.haoke.dubbo.server.api;
