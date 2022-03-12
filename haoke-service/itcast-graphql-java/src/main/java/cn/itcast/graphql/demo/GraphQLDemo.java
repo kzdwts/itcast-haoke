ddc2c7d6ccf22551375295bc5e8de4e4bd6f56be
kangyong <kangyong_wts@sina.com>
2022-03-12 20:08:17 +0800

    feat: 定义查询的类型

--- haoke-service/itcast-graphql-java/src/main/java/cn/itcast/graphql/demo/GraphQLDemo.java
+++ haoke-service/itcast-graphql-java/src/main/java/cn/itcast/graphql/demo/GraphQLDemo.java
@@ -1,8 +1,10 @@
 package cn.itcast.graphql.demo;
 
+import cn.itcast.graphql.vo.User;
 import graphql.schema.GraphQLFieldDefinition;
 import graphql.schema.GraphQLObjectType;
 import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
 
 import static graphql.schema.GraphQLObjectType.newObject;
 
@@ -30,5 +32,26 @@ public class GraphQLDemo {
     public static GraphQLSchema createGraphqlSchema(GraphQLFieldDefinition userDefinition) {
         GraphQLObjectType userQuery = newObject().name("userQuery").field(userDefinition).build();
         return GraphQLSchema.newSchema().query(userQuery).build();
+    }
+
+    /**
+     * 定义查询的类型
+     * <p>
+     * type UserQuery {
+     *      user : User # 指定对象
+     * }
+     *
+     * @param userType {@link GraphQLObjectType}
+     * @return {@link GraphQLFieldDefinition}
+     * @author Kang Yong
+     * @date 2022/3/12
+     */
+    public static GraphQLFieldDefinition createuserDefinition(GraphQLObjectType userType) {
+        return GraphQLFieldDefinition.newFieldDefinition()
+                .name("user")
+                .type(userType)
+                // 静态数据
+                .dataFetcher(new StaticDataFetcher(new User(1L, "张三", 20)))
+                .build();
     }
 }
