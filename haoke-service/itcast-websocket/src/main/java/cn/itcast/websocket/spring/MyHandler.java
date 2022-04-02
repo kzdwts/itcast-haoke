b743c57099e5414f9292b4124b891a262cc32b93
kangyong <kangyong_wts@sina.com>
2022-04-02 23:31:21 +0800

    feat: springboot集成websocket

---
+++ haoke-service/itcast-websocket/src/main/java/cn/itcast/websocket/spring/MyHandler.java
@@ -0,0 +1,13 @@
+package cn.itcast.websocket.spring;
+
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+/**
+ * websocket 处理
+ *
+ * @author Kang Yong
+ * @date 2022/4/2
+ * @since 1.0.0
+ */
+public class MyHandler extends TextWebSocketHandler {
+}
