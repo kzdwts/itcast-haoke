7a2b23c88bff4fd1fa5081580078d0d2b7cb9df0
kangyong <kangyong_wts@sina.com>
2022-04-02 23:44:06 +0800

    feat: springboot集成websocket

---
+++ haoke-service/itcast-websocket/src/main/java/cn/itcast/websocket/MyWebSocketApplication.java
@@ -0,0 +1,20 @@
+package cn.itcast.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author Kang Yong
+ * @date 2022/4/2
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class MyWebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWebSocketApplication.class, args);
+    }
+
+}
--- haoke-service/itcast-websocket/src/main/java/cn/itcast/websocket/spring/MyHandler.java
+++ haoke-service/itcast-websocket/src/main/java/cn/itcast/websocket/spring/MyHandler.java
@@ -1,5 +1,8 @@
 package cn.itcast.websocket.spring;
 
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 /**
@@ -10,4 +13,52 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
  * @since 1.0.0
  */
 public class MyHandler extends TextWebSocketHandler {
+
+    /**
+     * 处理文本消息
+     *
+     * @param session {@link WebSocketSession}
+     * @param message {@link TextMessage}
+     * @author Kang Yong
+     * @date 2022/4/2
+     */
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        System.out.println("获取到消息》》" + message.getPayload());
+
+        session.sendMessage(new TextMessage("消息已收到"));
+
+        if (message.getPayload().equals("10")) {
+            for (int i = 0; i < 10; i++) {
+                session.sendMessage(new TextMessage("消息 -》 " + i));
+
+                Thread.sleep(100);
+            }
+        }
+    }
+
+    /**
+     * 建立连接
+     *
+     * @param session {@link WebSocketSession}
+     * @author Kang Yong
+     * @date 2022/4/2
+     */
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        session.sendMessage(new TextMessage("欢迎连接到ws服务"));
+    }
+
+    /**
+     * 断开连接
+     *
+     * @param session {@link WebSocketSession}
+     * @param status  {@link CloseStatus}
+     * @author Kang Yong
+     * @date 2022/4/2
+     */
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        System.out.println("断开链接");
+    }
 }
