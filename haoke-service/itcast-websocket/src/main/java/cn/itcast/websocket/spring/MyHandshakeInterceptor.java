3ed9628923ce0bf4848f7724f39e1289797316d5
kangyong <kangyong_wts@sina.com>
2022-04-03 23:17:07 +0800

    feat: 握手拦截器

---
+++ haoke-service/itcast-websocket/src/main/java/cn/itcast/websocket/spring/MyHandshakeInterceptor.java
@@ -0,0 +1,30 @@
+package cn.itcast.websocket.spring;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+
+/**
+ * 握手拦截器
+ *
+ * @author Kang Yong
+ * @date 2022/4/3
+ * @since 1.0.0
+ */
+@Component
+public class MyHandshakeInterceptor implements HandshakeInterceptor {
+
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Map<String, Object> map) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Exception e) {
+
+    }
+}
