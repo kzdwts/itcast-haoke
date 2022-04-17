9e7652bd1fdf787e5da28626f275fca4b58d1a98
kangyong <kangyong_wts@sina.com>
2022-04-17 22:51:16 +0800

    feat: 发送消息测试

--- haoke-service/itcast-rocketmq/src/main/java/cn/itcast/rocketmq/SyncProducer.java
+++ haoke-service/itcast-rocketmq/src/main/java/cn/itcast/rocketmq/SyncProducer.java
@@ -26,25 +26,25 @@ public class SyncProducer {
         producer.start();
 
         // 发送消息
-        for (int i = 0; i < 100; i++) {
-            Message msg = new Message("TopicTest11",
-                    "TagA",
-                    ("Hello RoekcetMQ" + i).getBytes(RemotingHelper.DEFAULT_CHARSET)
-            );
-
-            SendResult sendResult = producer.send(msg);
-            System.out.printf("%s%n ", sendResult);
-        }
-
-//        String msgStr = "用户A发送消息给用户B";
-//        Message msg = new Message("haoke_im_topic", "SEND_MSG", msgStr.getBytes(RemotingHelper.DEFAULT_CHARSET));
-//        // 发送消息
-//        SendResult sendResult = producer.send(msg);
+//        for (int i = 0; i < 100; i++) {
+//            Message msg = new Message("TopicTest11",
+//                    "TagA",
+//                    ("Hello RoekcetMQ" + i).getBytes(RemotingHelper.DEFAULT_CHARSET)
+//            );
 //
-//        System.out.println("消息状态：" + sendResult.getSendStatus());
-//        System.out.println("消息id：" + sendResult.getMsgId());
-//        System.out.println("消息queue：" + sendResult.getMessageQueue());
-//        System.out.println("消息offset：" + sendResult.getQueueOffset());
+//            SendResult sendResult = producer.send(msg);
+//            System.out.printf("%s%n ", sendResult);
+//        }
+
+        String msgStr = "用户A发送消息给用户B";
+        Message msg = new Message("haoke_im_topic", "SEND_MSG", msgStr.getBytes(RemotingHelper.DEFAULT_CHARSET));
+        // 发送消息
+        SendResult sendResult = producer.send(msg);
+
+        System.out.println("消息状态：" + sendResult.getSendStatus());
+        System.out.println("消息id：" + sendResult.getMsgId());
+        System.out.println("消息queue：" + sendResult.getMessageQueue());
+        System.out.println("消息offset：" + sendResult.getQueueOffset());
 
         producer.shutdown();
     }
