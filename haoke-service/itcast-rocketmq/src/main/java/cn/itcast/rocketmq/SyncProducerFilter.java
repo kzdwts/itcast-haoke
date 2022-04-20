690bbdf51e8091628fe1482e1066af9228dc195a
kangyong <kangyong_wts@sina.com>
2022-04-20 22:06:18 +0800

    feat: 消息过滤器

---
+++ haoke-service/itcast-rocketmq/src/main/java/cn/itcast/rocketmq/SyncProducerFilter.java
@@ -0,0 +1,16 @@
+package cn.itcast.rocketmq;
+
+/**
+ * 消息过滤器
+ *
+ * @author Kang Yong
+ * @date 2022/4/20
+ * @since 1.0.0
+ */
+public class SyncProducerFilter {
+
+    public static void main(String[] args) {
+
+    }
+
+}
