840024a26456c04a497b5a3bd44f9b899985563b
kangyong <kangyong_wts@sina.com>
2022-04-21 23:09:28 +0800

    feat: 消息过滤器

--- haoke-service/itcast-rocketmq/src/main/java/cn/itcast/rocketmq/SyncProducerFilter.java
+++ haoke-service/itcast-rocketmq/src/main/java/cn/itcast/rocketmq/SyncProducerFilter.java
@@ -1,5 +1,16 @@
 package cn.itcast.rocketmq;
 
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
 /**
  * 消息过滤器
  *
@@ -9,8 +20,27 @@ package cn.itcast.rocketmq;
  */
 public class SyncProducerFilter {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MQClientException, UnsupportedEncodingException, MQBrokerException, RemotingException, InterruptedException {
+        DefaultMQProducer producer = new DefaultMQProducer("HAOKE_IM");
+        producer.setNamesrvAddr("192.168.100.134:9876");
+        producer.start();
 
+        String msgStr = "美女001";
+        Message msg = new Message("haoke_meinv_topic", "SEND_MSG", msgStr.getBytes(RemotingHelper.DEFAULT_CHARSET));
+        msg.putUserProperty("age", "18");
+        msg.putUserProperty("sex", "女");
+
+        // 发送消息
+        SendResult sendResult = producer.send(msg);
+
+        System.out.println("消息状态：" + sendResult.getSendStatus());
+        System.out.println("消息id：" + sendResult.getMsgId());
+        System.out.println("消息queue：" + sendResult.getMessageQueue());
+        System.out.println("消息offset：" + sendResult.getQueueOffset());
+
+        System.out.println(sendResult);
+
+        producer.shutdown();
     }
 
 }
