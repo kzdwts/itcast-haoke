e8e769a275826408428604620d5551aa7d6f3aed
kangyong <kangyong_wts@sina.com>
2022-04-23 23:38:04 +0800

    feat: 接收消息

--- haoke-service/itcast-rocketmq/src/main/java/cn/itcast/rocketmq/ConsumerFilterDemo.java
+++ haoke-service/itcast-rocketmq/src/main/java/cn/itcast/rocketmq/ConsumerFilterDemo.java
@@ -1,5 +1,16 @@
 package cn.itcast.rocketmq;
 
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.MessageSelector;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
 /**
  * 接受消息
  *
@@ -9,8 +20,30 @@ package cn.itcast.rocketmq;
  */
 public class ConsumerFilterDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("HAOKE_IM");
+        consumer.setNamesrvAddr("192.168.100.134:9876");
 
+        // 订阅topic，接收此Topic下的所有消息
+        consumer.subscribe("haoke_meinv_topic", MessageSelector.bySql("age>=20 AND sex='女'"));
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                for (MessageExt msg : msgs) {
+                    try {
+                        System.out.println(new String(msg.getBody(), "UTF-8"));
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("收到消息->" + msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
+
+        // 加入broker的配置文件中
+        // enablePropertyFilter=true
     }
 
 }
