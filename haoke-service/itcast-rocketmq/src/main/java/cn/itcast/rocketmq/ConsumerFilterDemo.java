77b4f8bd7e0f28314766a506e3a3f1f533c39ca6
kangyong <kangyong_wts@sina.com>
2022-04-23 23:33:33 +0800

    feat: 消息过滤器

---
+++ haoke-service/itcast-rocketmq/src/main/java/cn/itcast/rocketmq/ConsumerFilterDemo.java
@@ -0,0 +1,16 @@
+package cn.itcast.rocketmq;
+
+/**
+ * 接受消息
+ *
+ * @author Kang Yong
+ * @date 2022/4/21
+ * @since 1.0.0
+ */
+public class ConsumerFilterDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+}
