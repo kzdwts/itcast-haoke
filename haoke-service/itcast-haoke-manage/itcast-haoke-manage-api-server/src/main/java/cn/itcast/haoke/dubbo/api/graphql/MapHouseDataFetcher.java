6cfc04268bc7518d3316037ce9008f8b11929640
kangyong <kangyong_wts@sina.com>
2022-05-11 23:28:04 +0800

    feat: 查询房子地图数据

--- haoke-service/itcast-haoke-manage/itcast-haoke-manage-api-server/src/main/java/cn/itcast/haoke/dubbo/api/graphql/MapHouseDataFetcher.java
+++ haoke-service/itcast-haoke-manage/itcast-haoke-manage-api-server/src/main/java/cn/itcast/haoke/dubbo/api/graphql/MapHouseDataFetcher.java
@@ -1,12 +1,9 @@
 package cn.itcast.haoke.dubbo.api.graphql;
 
-import cn.itcast.haoke.dubbo.api.vo.map.MapHouseDataResult;
-import cn.itcast.haoke.dubbo.api.vo.map.MapHouseXY;
+import cn.itcast.haoke.dubbo.api.service.MongoHouseService;
 import graphql.schema.DataFetchingEnvironment;
-import lombok.Data;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * 地图数据查询
@@ -15,8 +12,11 @@ import java.util.List;
  * @date 2022/5/11
  * @since 1.0.0
  */
-@Data
+@Component
 public class MapHouseDataFetcher implements MyDataFetcher {
+
+    @Autowired
+    private MongoHouseService mongoHouseService;
 
     /**
      * 查询名称
@@ -40,18 +40,13 @@ public class MapHouseDataFetcher implements MyDataFetcher {
      */
     @Override
     public Object dataFetcher(DataFetchingEnvironment environment) {
-        List<MapHouseXY> list = new ArrayList<>();
-        list.add(new MapHouseXY(116.43244f, 39.929986f));
-        list.add(new MapHouseXY(116.424355f, 39.92982f));
-        list.add(new MapHouseXY(116.423349f, 39.935214f));
-        list.add(new MapHouseXY(116.350444f, 39.931645f));
-        list.add(new MapHouseXY(116.351684f, 39.91867f));
-        list.add(new MapHouseXY(116.353983f, 39.913855f));
-        list.add(new MapHouseXY(116.357253f, 39.923152f));
-        list.add(new MapHouseXY(116.349168f, 39.923152f));
-        list.add(new MapHouseXY(116.36232f, 39.938339f));
-        list.add(new MapHouseXY(116.374249f, 39.94625f));
-        list.add(new MapHouseXY(116.380178f, 39.953053f));
-        return new MapHouseDataResult(list);
+        Float lng = ((Double) environment.getArgument("lng")).floatValue();
+        Float lat = ((Double) environment.getArgument("lat")).floatValue();
+
+        Integer zoom = environment.getArgument("zoom");
+
+        System.out.println("lng->" + lng + ",lat->" + lat + ",zoom->" + zoom);
+
+        return this.mongoHouseService.queryHouseData(lng, lat, zoom);
     }
 }
